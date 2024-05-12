bd9068646f942f5d6f13b34400783f3f809a7054
zzzzzzzzzzzzzzzzz <155483815+RunAccount1@users.noreply.github.com>
2024-05-12 15:23:09 -0400

    Create utils.lua

---
+++ Libraries/utils.lua
@@ -0,0 +1,29 @@
+local utils = {}
+
+Players = game:GetService("Players")
+UserInputService = game:GetService("UserInputService")
+LocalPlayer = Players.LocalPlayer
+Character = LocalPlayer.Character
+Humanoid = Character.Humanoid
+
+utils.onGround = function()
+	return Humanoid.FloorMaterial ~= Enum.Material.Air
+end
+utils.isMoving = function()
+	if onMobile then
+		return Humanoid.MoveDirection ~= Vector3.zero
+	end
+	return UserInputService:IsKeyDown("W") or UserInputService:IsKeyDown("A") or UserInputService:IsKeyDown("S") or UserInputService:IsKeyDown("D")
+end
+utils.newChat = function(Message)
+  DefaultChatSystemChatEvents:WaitForChild("SayMessageRequest"):FireServer(Message, "All")
+end
+utils.NewRaycast = function(Start, Direction)
+  local p = RaycastParams.new()
+	p.FilterType = Enum.RaycastFilterType.Exclude
+	p.FilterDescendantsInstances = {Character,workspace.CurrentCamera}
+	local ray = workspace:Raycast(Start,Direction,p)
+	return ray
+end
+
+return utils
