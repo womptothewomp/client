6d97f83362dc29033e2c6856e4c5f1986a58d33e
zzzzzzzzzzzzzzzzz <155483815+RunAccount1@users.noreply.github.com>
2024-05-14 21:44:09 -0400

    Create AmbienceColors.lua

---
+++ MainTables/AmbienceColors.lua
@@ -0,0 +1,10 @@
+local AmbienceTable = {
+	Purple = Color3.fromRGB(100, 0, 255),
+	Blue = Color3.fromRGB(0, 20, 255),
+	Green = Color3.fromRGB(0, 255, 30),
+	Yellow = Color3.fromRGB(255, 255, 0),
+	Orange = Color3.fromRGB(255, 140, 25),
+	Red = Color3.fromRGB(255, 0, 0),
+	Brown = Color3.fromRGB(120, 40, 15),
+}
+return AmbienceTable
