a32156c76da62e1ccb088bf68f42f6c98e4da1e7
zzzzzzzzzzzzzzzzz <155483815+RunAccount1@users.noreply.github.com>
2024-05-14 20:35:11 -0400

    Create AnimationTable.lua

---
+++ Bedwars/AnimationTable.lua
@@ -0,0 +1,11 @@
+local funAnimations = {
+	PLAYER_VACUUM_SUCK = "rbxassetid://9671620809",
+	WINTER_BOSS_SPAWN = "rbxassetid://11843861791",
+	GLUE_TRAP_FLYING = "rbxassetid://11466075174",
+	VOID_DRAGON_TRANSFORM = "rbxassetid://10967424821",
+	SIT_ON_DODO_BIRD = "http://www.roblox.com/asset/?id=2506281703",
+	DODO_BIRD_FALL = "rbxassetid://7617326953",
+	SWORD_SWING = "rbxassetid://7234367412",
+}
+
+return funAnimations
