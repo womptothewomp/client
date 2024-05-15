6e85ff78e0245e64979662d06a217b84e7f7c476
zzzzzzzzzzzzzzzzz <155483815+RunAccount1@users.noreply.github.com>
2024-05-14 20:55:56 -0400

    Create Images.lua

---
+++ MainTables/Images.lua
@@ -0,0 +1,15 @@
+local assetTable = {
+	Sus = "http://www.roblox.com/asset/?id=9145833727",
+	Damc = "rbxassetid://16930990336",
+	Springs = "rbxassetid://16930908008",
+	Xylex = "rbxassetid://16930961099",
+	Alsploit = "http://www.roblox.com/asset/?id=12772788813",
+	Matrix = "http://www.roblox.com/asset/?id=1412150157",
+	Covid = "http://www.roblox.com/asset/?id=8518879821",
+	Space = "http://www.roblox.com/asset/?id=2609221356",
+	Windows = "http://www.roblox.com/asset/?id=472001646",
+	Trol = "http://www.roblox.com/asset/?id=6403436054",
+	Cat = "http://www.roblox.com/asset/?id=14841615129",
+	Furry = "http://www.roblox.com/asset/?id=14831068996",
+}
+return assetTable
