2402b221db45d2637a565761f226c4b138d06c7b
VelocityCheats <160363547+VelocityCheats@users.noreply.github.com>
2024-07-05 14:22:48 -0400

    Update MainScript.lua

--- MainScript.lua
+++ MainScript.lua
@@ -1313,11 +1313,11 @@ Speed = Motion.NewButton({
 				end
 
 				if SpeedMode.Option == "pulse_WalkSpeed" then
-					game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = math.random(20, 23)																
+					game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = math.random(23, 25)																
 				end
 
 				if SpeedMode.Option == "pulse_CFrame" then
-					PrimaryPart.CFrame += (math.random(0.4, 0.6) * dir)																
+					PrimaryPart.CFrame += (math.random(0.5, 0.7) * dir)																
 				end																
 				task.wait()
 			until not Speed.Enabled
